c53fe20e27e911c746d7eaea861ec7e46b39d519
aarontao1990 <aarontao1990@gmail.com>
2015-03-06 21:49:27 +0800

    add chat and echo_server

---
+++ echo_server/echo_server.py
@@ -0,0 +1,43 @@
+from twisted.internet.protocol import Protocol, Factory
+from twisted.internet.endpoints import TCP4ServerEndpoint
+from twisted.internet import reactor
+
+class Echo(Protocol):
+
+    def dataReceived(self, data):
+        self.transport.write(data)
+
+class Quote(Protocol):
+
+    def connectionMade(self):
+        self.transport.write("An apple a day keeps doctor away\n")
+        self.transport.loseConnection()
+
+
+class CountedEcho(Protocol):
+
+    def __init__(self, factory):
+        self.factory = factory
+
+    def connectionMade(self):
+        self.factory.numProtocols = self.factory.numProtocols + 1
+        self.transport.write("Welecome! There are now %d protocols connected"
+                             % self.factory.numProtocols)
+
+
+    def connectionLost(self, reason):
+        self.factory.numProtocols = self.factory.numProtocols - 1
+
+    def dataReceived(self, data):
+        self.transport.write(data)
+
+class QOTFactory(Factory):
+
+    def buildProtocol(self, addr):
+        return Quote()
+
+
+endpoint = TCP4ServerEndpoint(reactor, 8007)
+endpoint.listen(QOTFactory())
+reactor.run()
+
