96b3185a251d02dfa4fb35527004e2e47ecc14f3
aarontao1990 <aarontao1990@gmail.com>
2015-03-19 13:29:31 +0800

    add quote server

---
+++ quote/quote_server.py
@@ -0,0 +1,39 @@
+from twisted.internet.protocol import Factory, Protocol
+from twisted.internet import reactor
+
+class QuoteProtocol(Protocol):
+
+    def __init__(self, factory):
+        self.factory = factory
+
+    def dataReceived(self, data):
+        print '%d connections connected for now' % (self.factory.numConnections, )
+        print '> received : %s> response : %s' % (data, self.getQuote())
+        self.transport.write(self.getQuote())
+        self.updateQuote(data)
+
+
+    def connectionMade(self):
+        self.factory.numConnections += 1
+
+    def connectionLost(self, reason):
+        self.factory.numConnections -= 1
+
+    def getQuote(self):
+        return self.factory.quote
+
+    def updateQuote(self, quote):
+        self.factory.quote = quote
+
+
+class QuoteFactory(Factory):
+    numConnections = 0
+
+    def __init__(self, quote=None):
+        self.quote = quote or "An apple a day keeps doctor away"
+
+    def buildProtocol(self, addr):
+        return QuoteProtocol(self)
+
+reactor.listenTCP(8000, QuoteFactory())
+reactor.run()
